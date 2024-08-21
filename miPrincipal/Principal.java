8e02693057d5bc86cbb450f6b76d1681f521188e
JulioAbitia <146040773+JulioAbitia@users.noreply.github.com>
2024-08-21 15:48:27 +0000

    Actualizacion HOLA MUNDO

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -2,7 +2,7 @@ package miPrincipal;
 
 public class Principal {
     public String getGreeting() {
-        return "Hello World!";
+        return "HOLA MUNDO!";
     }
 
     public static void main(String[] args) {

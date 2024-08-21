906bd4df69543e1d23312b1568039433d98e3818
JulioAbitia <146040773+JulioAbitia@users.noreply.github.com>
2024-08-21 16:01:39 +0000

    Nuevas Clases

---
+++ miPrincipal/Fecha.java
@@ -0,0 +1,8 @@
+package miPrincipal;
+    public class Fecha 
+    {
+        
+        
+    }
+
+
---
+++ miPrincipal/Persona.java
@@ -0,0 +1,8 @@
+package miPrincipal;
+public class Persona 
+{
+    private String Dni;
+    private String Nombre;
+    private Fecha FechaNacimiento;
+
+}
